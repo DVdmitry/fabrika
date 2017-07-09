e7d4d0ef981e712d941e4a93ffb6bd51d80984b8
Dmitry <isaevski.dv@gmail.com>
2017-07-09 19:01:43 +0300

    third com

--- src/app/carousel-basic/picture-data.ts
+++ src/app/carousel-basic/picture-data.ts
@@ -1,7 +1,6 @@
 import { Picture } from './picture-carousel';
 
 export const PICTURES: Picture[] = [
-
     new Picture("https://s3.amazonaws.com/fabrika-kurs/carousel/books.jpg", "На основе материалов Национальной бибилиотеки", "материалы Интернета - используются только официальные сайты (Статистический комитет, Национальный банк и т.д.)"),
     new Picture("https://s3.amazonaws.com/fabrika-kurs/carousel/library.jpg", "Доработка до защиты", "осуществляется в течение 3-4 дней по курсовым и 5-7 дней по дипломным")
 ]
--- src/app/vk/vk.component.ts
+++ src/app/vk/vk.component.ts
@@ -1,9 +1,8 @@
 import { Component } from "@angular/core";
 @Component({
     selector: 'vk',
-    templateUrl: './vk.component.'
+    templateUrl: './vk.component.html'
 })
 
 export class VKComponent{
-
 }
